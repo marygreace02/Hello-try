a66804520c52d734b44642d7e43c405f7353dd61
marygreace02 <maryginfae08@gmail.com>
2025-07-20 16:56:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("hi te shai and gie.....")
+alert("nagtry lang ko ug code......")
+alert("one question lang.")
+alert("how is IT Journey?")
+alert(" is it good or bad?")
+alert("anyways javascript gamit nako ani HAHAHAHHAHAHAH")
